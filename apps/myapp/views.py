034cf7d5f8b298892b66abebd73959239e03a0d1
Murakasi <murakasi.ruslan@gmail.com>
2026-03-21 20:31:15 +0100

    add in views.py index method and in global templates base.html"

--- apps/myapp/views.py
+++ apps/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpRequest,HttpResponse
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'name': 'Ruslan',
+        'age': 49,
+        'email': 'bigun.ruslan@gmail.com'
+    }
+    return render(request, 'base.html', context)
