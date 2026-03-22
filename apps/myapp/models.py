e2bd7bd773db7aca6bd734c8f2b6f5a5c8f43d79
Murakasi <murakasi.ruslan@gmail.com>
2026-03-22 09:35:35 +0100

    create Cuisine and MenuItem models in myapp/models.py

--- apps/myapp/models.py
+++ apps/myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Cousine(models.TextChoices):
+    ITALY = 'italy', "Italy"
+    FRENCH = 'french', 'French'
+    UKRAINE = 'ukraine', 'Ulraine'
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    couisine = models.CharField(max_length=20, choices=Cousine, default=Cousine.ITALY)
+
+    def __str__(self):
+        return f'{self.title} : {self.price}'
+
+    class Meta:
+        db_table = 'menu'
+        verbosa_name = 'menu'
+        verbose_name_plural = 'menus'
+        ordering = ['-price']
