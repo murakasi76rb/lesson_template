97a9a71f3099628981cdba0c9edc694fa15658fd
Murakasi <murakasi.ruslan@gmail.com>
2026-03-22 09:49:58 +0100

    Add menu method in myapp/views.py

--- apps/myapp/views.py
+++ apps/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from apps.myapp.models import MenuItem
 # Create your views here.
 from django.http import HttpRequest,HttpResponse
 
@@ -10,3 +10,12 @@ def index(request: HttpRequest) -> HttpResponse:
         'email': 'bigun.ruslan@gmail.com'
     }
     return render(request, 'myapp/index.html', context)
+
+
+
+def menu(request:HttpRequest) -> HttpResponse:
+    menu = MenuItem.objects.all()
+    context = {
+        'menu': menu,
+    }
+    return render(request, 'myapp/menu.html', context)
